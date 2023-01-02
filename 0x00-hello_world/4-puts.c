a0a1ea712ea39ecb084238a6d64bacb048a86e66
kolaolori <kolaolori@gmail.com>
2023-01-01 23:55:31 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * write a c program that prints 
+ * followed by a new line
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
