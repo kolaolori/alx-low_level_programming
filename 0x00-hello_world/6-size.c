c1f00945cb3f673c9b55ebe62926d5218391336c
kolaolori <kolaolori@gmail.com>
2023-01-03 05:36:53 -0800

    task6.3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
 	return (0);
 
 }
