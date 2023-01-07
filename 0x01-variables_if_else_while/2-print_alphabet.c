3d93e9de875ea8295fc3a1d2084d3615b38c7bdc
kolaolori <kolaolori@gmail.com>
2023-01-06 23:25:14 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+		return (0);
