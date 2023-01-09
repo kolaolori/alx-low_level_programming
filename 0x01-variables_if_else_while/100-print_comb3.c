560952c8b42976c36e275ff88b8b3b614f7d7670
kolaolori <kolaolori@gmail.com>
2023-01-08 23:09:44 -0800

    task10a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
 	{
-		for (ones = '0'; ones <= '0'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '0' && tens == '0'))/*adds comma and space*/
+				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
