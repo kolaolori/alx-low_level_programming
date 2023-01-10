05b00a1d27e94fee02f9ed8200f52d8cb59180f9
kolaolori <kolaolori@gmail.com>
2023-01-10 09:34:32 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char x10;
+
+	while (count++ <= 9)
+	{
+		for  (x10 = 'a'; x10 <= 'z'; x10++)
+		_putchar(x10);
+		_putchar('\n');
+	}
