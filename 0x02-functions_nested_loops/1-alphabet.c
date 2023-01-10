7efa4d57f172877e63b254071527f259357a9c6a
kolaolori <kolaolori@gmail.com>
2023-01-10 09:01:33 -0800

    task1c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - print_alphabet
+ * print_alphabet - print letters
  * Return: 0
  */
 
 void print_alphabet(void)
+
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		_putchar(c);
